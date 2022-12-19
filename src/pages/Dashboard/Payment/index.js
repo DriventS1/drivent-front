df0ee84acaee63f0e39ec8e64eb8ec32a7a5b14d
Jansenck <caik11@hotmail.com>
2022-12-18 23:22:25 -0300

    feat: add credit card payment method to dashboard

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,94 +1,9 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import { Radio } from 'antd';
-import PaymentForm from '../../../components/Payment/Payment';
+import TicketType from './ticketType';
 
 export default function Payment() {
-// A tabela ticketTypes precisa ser povoada pelas infos abaixo
-  const types = [
-    {
-      id: 3,
-      name: 'Online',
-      price: 100,
-      isRemote: true,
-      includesHotel: false,
-      createdAt: '2022-12-16T02:12:42.501Z',
-      updatedAt: '2022-12-16T02:12:42.501Z'
-    },
-    {
-      id: 2,
-      name: 'Presencial',
-      price: 600,
-      isRemote: false,
-      includesHotel: true,
-      createdAt: '2022-12-16T02:12:42.501Z',
-      updatedAt: '2022-12-16T02:12:42.501Z'
-    },
-    {
-      id: 1,
-      name: 'Presencial',
-      price: 250,
-      isRemote: false,
-      includesHotel: false,
-      createdAt: '2022-12-16T02:12:42.501Z',
-      updatedAt: '2022-12-16T02:12:42.501Z'
-    }
-  ];
-
-  const [valueId, setValueId] = useState('');
-  const [valueHotel, setValueHotel] = useState('');
-  const [showOptions, setShowOptions] = useState(false);
-
   return (
-    <Container>
-      <div className='title'>Ingresso e pagamento</div>
-      <PaymentForm/>
-    </Container>
+    <>
+      <TicketType/>
+    </>
   );
 }
-
-// Com o valueId escolhido uma function poderá enviar esse número como ticketId para criar o ticket do usuário
-
-const Container = styled.div`
-
-.title {
-  font-size: 30px;
-  color: black;
-  margin-bottom: 40px;
-}
-
-.subtitle {
-  font-size: 20px;
-  color: gray;
-  margin: 10px 0 10px 0;
-}
-
-.buttons {
-  display: flex;
-  flex-direction: row;
-}
-
-.each-button {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 145px;
-  height: 145px;
-  border: 1px solid #CECECE;
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-  border-radius: 20px;
-  margin:  10px 20px 10px 0;
-}
-
-.type {
-  color: black;
-  margin: 3px;
-}
-
-.price {
-  color: gray;
-  margin: 3px;
-}
-
-`;
---
+++ src/pages/Dashboard/Payment/ticketType.js
@@ -0,0 +1,134 @@
+import { useContext, useState } from 'react';
+import styled from 'styled-components';
+import UserContext from '../../../contexts/UserContext';
+import useTicketType from '../../../hooks/api/useTicketType';
+import { Radio } from 'antd';
+
+export default function TicketType() {
+  const { userData: data } = useContext(UserContext);  
+  const { ticketType } =  useTicketType(data.user.id);
+
+  const [valueId, setValueId] = useState('');
+  const [valueHotel, setValueHotel] = useState('');
+  const [showOptions, setShowOptions] = useState(false);
+
+  return (
+    <>
+      <Title>
+        Ingresso e pagamento
+      </Title>
+
+      <Subtitle>
+        Primeiro, escolha sua modalidade de ingresso
+      </Subtitle>
+
+      <Radio.Group onChange={e => setValueId(e.target.value)} value={valueId}>
+        <Buttons>
+
+          {ticketType?.map(type => {
+            return(
+              !type.includesHotel? 
+                type.isRemote ? 
+                  <EachButton>
+
+                    <Radio value={type.id} onChange={e => setShowOptions(false)}>
+                      <div className='type'>
+                        {type.name}
+                      </div>
+                      <div className='price'>+ R${type.price}</div>
+                    </Radio>
+
+                  </EachButton>
+                  :
+                  <EachButton>
+
+                    <Radio value={0} onChange={e => setShowOptions(true)}>
+                      <div className='type'>
+                        {type.name}
+                      </div>
+                      <div className='price'>+ R${type.price}</div>
+                    </Radio>
+
+                  </EachButton>
+                : 
+                ('')
+            );
+          })}
+        </Buttons>
+      </Radio.Group>
+
+      {showOptions ? (<> 
+        <Subtitle>
+            Ótimo, agora escolha sua modalidade de hospedagem
+        </Subtitle>
+
+        <Radio.Group onChange={e => setValueHotel(e.target.value)} value={valueHotel}>
+
+          <Buttons>
+            {ticketType?.map(type => {
+              return(
+                !type.isRemote?
+                  <EachButton>
+
+                    <Radio value={type.id}>
+                      <div className='type'>
+                        { !type.isRemote && type.includesHotel? 'Com Hotel' : 'Sem Hotel'}
+                      </div>
+                      <div className='price'>+ R${type.price - 250}</div>
+                    </Radio>
+
+                  </EachButton>
+                  :
+                  ('')
+              );
+            })}
+          </Buttons>
+
+        </Radio.Group>
+      </>) : ('')}
+    </>
+  );
+}
+
+// Com o valueId escolhido uma function poderá enviar esse número como ticketId para criar o ticket do usuário
+
+const Title = styled.div`
+  font-size: 30px;
+  color: black;
+  margin-bottom: 40px;
+`;
+
+const Subtitle = styled.div`
+  font-size: 20px;
+  color: gray;
+  margin: 10px 0 10px 0;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  .type {
+  color: black;
+  margin: 3px;
+}
+
+  .price {
+  color: gray;
+  margin: 3px;
+}
+`;
+
+const EachButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 145px;
+  height: 145px;
+  border: 1px solid #CECECE;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 20px;
+  margin:  10px 20px 10px 0;
+`;
+
